4bb40d5b1d1d0ca84127d6fdbcfbdf6886c50cf8
Jessica <jessicacristina.recife@gmail.com>
2023-09-06 22:55:32 -0300

    projeto guiado 1

---
+++ exercicios/para-casa/projeto1.py
@@ -0,0 +1,78 @@
+# CRUD = CREATE, READE, UPDATE, DELETE
+
+
+import time
+
+lista_produtos = [
+    {"COD": 3, "nome": "BIGBIG", "preco": 1.0},
+    {"COD": 2, "nome": "Chokito", "preco": 1.0}, 
+    {"COD": 1, "nome": "Mentos", "preco": 4.0}, 
+    {"COD": 4, "nome": "Sonho de Valsa", "preco": 52.0}
+    ]
+
+cod_produto = 1
+def menu():
+    while True:
+        print("\n ** MENU LOJA REPROGRAMA **\n")
+        print("1 - Adicionar")
+        print("2 - Exibir detalhes")
+        print("3 - Atualizar")
+        print("4 - Apagar")
+        print("5 - Exibir todos")
+        print("0 - Sair")
+
+        opcao = input("Escolha a opção desejada\n")
+
+        if opcao == "1":
+            adicionar_produto()
+        elif opcao == "2":
+            print("opcao exibir detalhes doce")
+        elif opcao == "3":
+            atualizar_produto()
+        elif opcao == "4":
+            print("opcao apagar doce")
+        elif opcao == "5":
+            listar_todos()
+        elif opcao == "0":
+            break
+        else:
+            print("Opção inválida, por favor escolha uma opção do menu")
+
+def gerar_cod_produto():
+    if len(lista_produtos) == 0:
+        return 1
+    lista_produtos.sort(key=lambda produto: produto.get("COD"), reverse=True)
+    novo_cod = lista_produtos[0].get("COD") + 1
+
+    return novo_cod
+def adicionar_produto():
+    nome_produto = input("Digite o nome do produto:\n")
+    preco_produto = input("Digite o preço do produto:\n")
+
+    produto = {
+        "COD": gerar_cod_produto(),
+        "nome": nome_produto,
+        "preço": float(preco_produto),
+    }
+    lista_produtos.append(produto)
+
+    print(lista_produtos)
+
+
+
+def atualizar_produto():
+    cod_produto = input("Digite o código do produto para atualizar:\n")
+
+    for index in range(len(lista_produtos)):
+        if lista_produtos[index].get("COD") == int(cod_produto):
+            novo_valor = input("Digite o novo valor do produto:\n")
+            lista_produtos[index]["preço"] = float(novo_valor)
+            print(f"O produto foi atualizado com sucesso! {lista_produtos[index]}")
+
+
+def listar_todos():
+    for index in range(len(lista_produtos)):
+        print(f"{lista_produtos[index]}\n")
+
+
+menu()
