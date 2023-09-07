4f479b5a321cf9be1e7d6e68301de358df281a9c
Taianne <tairocha06@gmail.com>
2023-09-06 23:06:10 -0300

    Projeto Resolvido em Grupo

--- exercicios/para-sala/loja.py
+++ exercicios/para-sala/loja.py
@@ -3,14 +3,18 @@
 # Exibir detalhe de um doce
 # Atualizar doce
 # Apagar doces
-# Exibir a lista de todos os doces
-import time
 
+# Exibir a lista de todos os doces
+
+# removi o import time 
 lista_produtos = [{'id': 3, 'nome': 'asdasdas', 'preço': 32.0}, {'id': 2, 'nome': 'asd', 'preço': 2.0}, {'id': 1, 'nome': 'a', 'preço': 1.0}, {'id': 4, 'nome': '23123', 'preço': 13123.0}]
 
+# Importante definir o identificador do produto, para garantir que cada produto tenha sua identificação para as demais funcionalidades
 id_produto = 1
 def menu():
+    #Definir a função menu, para apresentar as demais funções do menu
     while True:
+    # para um controle de repetição, em caso que seja usado repetidamente até uma codição de parada seja usada atéque seja interrompida.
         print("\n ** MENU LOJA REPROGRAMA **\n")
         print("1 - Adicionar")
         print("2 - Exibir detalhes")
@@ -19,19 +23,22 @@ def menu():
         print("5 - Exibir todos")
         print("0 - Sair")
 
-        opcao = input("Escolha a opção desejada\n")
 
+        opcao = input("Escolha a opção desejada\n")
+        #interação para que o usuário interaja e "diga" a opção desejada
+        #Cada função abaixo, apresenta uma opção do menu do menu e executa de acordo com a escolha do usuário.
         if opcao == "1":
             adicionar_produto()
         elif opcao == "2":
-            print("opcao exibir detalhes doce")
+            exibir_detalhes()
         elif opcao == "3":
             atualizar_produto()
         elif opcao == "4":
-            print("opcao apagar doce")
+            delete()
         elif opcao == "5":
             listar_todos()
-        elif opcao == "0":
+        elif opcao == "0": 
+            #BREAK, ncessario no caso em tela para 'quebrar', 'interromper', 'pausar' o loopig
             break
         else:
             print("Opção inválida, por favor escolha uma opção do menu")
@@ -39,6 +46,7 @@ def menu():
 def gerar_id_produto():
     if len(lista_produtos) == 0:
         return 1
+    # será ultilizado .sort, é um método de lista que sera ultilizado para ordenar os elementos dessa lista. No caso em tela, está sendo usado para ordenar os produtos em `lista_produtos
     lista_produtos.sort(key=lambda produto: produto.get("id"), reverse=True)
     novo_id = lista_produtos[0].get("id") + 1
 
@@ -52,6 +60,7 @@ def adicionar_produto():
         "nome": nome_produto,
         "preço": float(preco_produto),
     }
+    #Deverá ser ultilizado append para adicionar ou "anexar" um elemento na lista_produtos.É o nome da lista à qual você deseja adicionar um elemento. A variável `lista_produtos` deve ser uma lista previamente definida em seu código. - `.append
     lista_produtos.append(produto)
 
     print(lista_produtos)
@@ -72,5 +81,21 @@ def listar_todos():
     for index in range(len(lista_produtos)):
         print(f"{lista_produtos[index]}\n")
 
+#exibir detalhes do menu
+def exibir_detalhes():
+    exibir_item = int(input("Digite a id:"))
+    
+    for index in lista_produtos:  
+        if exibir_item == index.get("id"): 
+           print(f"{index}")
+      
+def delete():
+    deletar_produto = int(input("Digite id que deseja deletar:"))
 
+    for index in range(len(lista_produtos)): 
+        if index < len(lista_produtos):     
+            if lista_produtos[index].get("id") == deletar_produto:
+                lista_produtos.pop(index)   
+                print("O produto foi excluído com sucesso!")
 menu()
+
